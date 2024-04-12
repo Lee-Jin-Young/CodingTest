2fe8516b50b5f5f14b34eba6e5da2c437c644d19
Lee JinYoung <129915982+Lee-Jin-Young@users.noreply.github.com>
2024-04-13 00:40:51 +0900

    [level 0] Title: 다항식 더하기, Time: 0.18 ms, Memory: 76.5 MB -BaekjoonHub

--- Java/프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
+++ Java/프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -5,32 +5,40 @@ class Solution {
 
         polynomial = polynomial.replaceAll(" [+] ", " ");
         String[] arr = polynomial.split(" ");
+
         for (String str : arr) {
             if (str.endsWith("x")) {
+                // x로 끝나는 항일 경우 x의 지수 추출
                 String temp = str.replace("x", "");
                 if (temp.isEmpty()) {
                     temp = "1";
                 }
                 x += Integer.parseInt(temp);
             } else {
+                // 상수항일 경우 계수 추출
                 n += Integer.parseInt(str);
             }
         }
 
-        String answer = "";
+        // 결과를 저장할 문자열 초기화
+        StringBuilder result = new StringBuilder();
+
+        // x의 지수가 0이 아니면 결과 문자열에 추가
         if (x != 0) {
-            if (x == 1) {
-                answer = answer + "x";
-            } else {
-                answer = answer + x + "x";
-            }
+            result.append(x == 1 ? "x" : x + "x");
         }
+
+        // x의 지수가 0이 아니고 상수항이 0이 아니면 결과 문자열에 "+" 추가
         if (x != 0 && n != 0) {
-            answer = answer + " + ";
+            result.append(" + ");
         }
+
+        // 상수항이 0이 아니면 결과 문자열에 추가
         if (n != 0) {
-            answer = answer + n;
+            result.append(n);
         }
-        return answer;
+
+        // 결과 문자열 반환
+        return result.toString();
     }
 }
